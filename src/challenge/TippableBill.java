b8caf73fd7c8e4333d0d49d19332b394fbcfdb7e
Kbusch54 <kevinbusch@comcast.net>
2019-08-07 09:09:10 -0400

    third commit

--- src/challenge/TippableBill.java
+++ src/challenge/TippableBill.java
@@ -8,27 +8,19 @@ public class TippableBill extends Bill {
 
 	public double tip;
 
-	
-
 	public TippableBill() {
 		super();
-		// TODO Auto-generated constructor stub
 	}
 
 	public TippableBill(double subtotal, double taxRate, double tip) {
 		super(subtotal, taxRate);
 		this.tip = tip;
-		
-		// TODO Auto-generated constructor stub
 	}
 
 	@Override
 	public double calcTotal() {
-		// TODO Auto-generated method stub
-		return super.calcTotal()+( tip);
+
+		return super.calcTotal() + (tip);
 	}
-	
-	
-	
-	
+
 }
